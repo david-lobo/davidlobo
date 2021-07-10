79444c30baf85b9fe21b68efdc0b38b4675bbc94
david lobo <david.lobo@trustedtrip.com>
2021-07-10 19:08:10 +0100

    Favicon added

--- public/index.php
+++ public/index.php
@@ -18,6 +18,12 @@ include_once($phpPath . '/helpers/data.php'); ?>
     <title>David Lobo - Software Engineer</title>
     <meta name="description" content="I'm David, a Software Engineer based in London. I’ve been working as a developer for over eight years.">
     <meta name="robots" content="noindex">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="https://unpkg.com/purecss@1.0.0/build/pure-min.css" integrity="sha384-nn4HPE8lTHyVtfCBi5yW9d20FjT8BJwUXyWZT9InLYax14RDjBj46LmSztkmNP9w" crossorigin="anonymous">
     <!--[if lte IE 8]>
